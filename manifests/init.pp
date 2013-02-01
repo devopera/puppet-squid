ce539a97e886d00a4538c64b761c71cb5cd9ca60
cleverlight <github.account@cleveraspaint.com>
2013-02-01 18:04:27 +0000

    untested, but basic CentOS install working

--- manifests/init.pp
+++ manifests/init.pp
@@ -1,33 +1,37 @@
-class squid( $localnet_src = '10.0.0.0/8',
-  $cache_mem = '256 MB',
-  $maximum_object_size_in_memory = '512 KB',
-  $memory_replacement_policy = 'lru',
-  $cache_replacement_policy = 'lru',
-  $cache_dir = '/var/spool/squid3',
-  $cache_dir_type = 'ufs',
-  $cache_dir_size = 100,
-  $maximum_object_size = '4096 KB',
-  $cache_swap_low = 90,
-  $cache_swap_high = 95,
-  $log_fqdn = off,
-  $cachemgr_passwd = disable,
-  $visible_hostname = undef,
-  $snmp_port = undef,
-  $adzapper = false ) {
+class squid(
+  $localnet_src = $squid::params::localnet_src,
+  $http_port = $squid::params::http_port,
+  $cache_mem = $squid::params::cache_mem,
+  $maximum_object_size_in_memory = $squid::params::maximum_object_size_in_memory,
+  $memory_replacement_policy = $squid::params::memory_replacement_policy,
+  $cache_replacement_policy = $squid::params::cache_replacement_policy,
+  $cache_dir = $squid::params::cache_dir,
+  $cache_dir_type = $squid::params::cache_dir_type,
+  $cache_dir_size = $squid::params::cache_dir_size,
+  $maximum_object_size = $squid::params::maximum_object_size,
+  $cache_swap_low = $squid::params::cache_swap_low,
+  $cache_swap_high = $squid::params::cache_swap_high,
+  $log_fqdn = $squid::params::log_fqdn,
+  $cachemgr_passwd = $squid::params::cachemgr_passwd,
+  $visible_hostname = $squid::params::visible_hostname,
+  $snmp_port = $squid::params::snmp_port,
+  $adzapper = $squid::params::adzapper,
+  $user = $squid::params::user,
+  $group = $squid::params::group,
+  $package_present = 'present',
+) inherits squid::params {
 
-  $package = 'squid3'
-  $service = 'squid3'
-  $user = 'proxy'
-  $group = 'proxy'
+  $package = $squid::params::package
+  $service = $squid::params::service
   $config_file = "/etc/${package}/squid.conf"
 
   package { $package:
-    ensure  => installed,
+    ensure  => $package_present,
   }
 
   if $adzapper {
     package { 'adzapper':
-      ensure  => installed,
+      ensure  => $package_present,
       require => Package[$package],
     }
   }
@@ -40,15 +44,26 @@ class squid( $localnet_src = '10.0.0.0/8',
     $ipv6 = true
   }
 
+  # create config file
   file { $config_file:
     ensure  => file,
     owner   => root,
     group   => root,
     mode    => '0644',
-    content => template("squid/${config_file}.erb"),
+    content => template("squid/etc/squid3/squid.conf.erb"),
     require => Package[$package],
   }
 
+  # create the user to own the proxy
+  user { "${user}":
+    ensure => present,
+    shell  => "/sbin/nologin",
+  }->
+  group { "${group}":
+    ensure => present,
+  }->
+
+  # create the directory for storing the cached data/packages
   file { $cache_dir:
     ensure  => directory,
     owner   => $user,
@@ -57,19 +72,31 @@ class squid( $localnet_src = '10.0.0.0/8',
     require => Package[$package],
   }
 
+  # restart service to use cache dir
   exec { 'Init cache dir':
-    command => "/usr/sbin/service ${service} stop && /usr/sbin/${service} -z",
+    path    => '/sbin:/usr/sbin',
+    command => "service ${service} stop && /usr/sbin/${service} -z",
     creates => "${cache_dir}/00",
     notify  => Service[$service],
     require => [ File[$cache_dir], File[$config_file] ],
   }
 
+  # setup the service properly
   service { $service:
     ensure    => running,
     enable    => true,
     require   => Package[$package],
-    restart   => '/etc/init.d/squid3 reload',
-    subscribe => File['/etc/squid3/squid.conf'],
+    restart   => "/etc/init.d/${service} reload",
+    subscribe => File[$config_file],
+  }
+
+  firewall { '03129 Squid Caching Service':
+    action => 'accept',
+    proto  => ['udp','tcp'],
+    dport  => $http_port,
+    notify  => Exec['persist-firewall'],
+    before  => Class['docommon::firewall::post'],
+    require => Class['docommon::firewall::pre'],
   }
 }
 
---
+++ manifests/params.pp
@@ -0,0 +1,39 @@
+class squid::params {
+  $localnet_src = '10.0.0.0/8'
+  $http_port = 3128
+  $cache_mem = '256 MB'
+  $maximum_object_size_in_memory = '512 KB'
+  $memory_replacement_policy = 'lru'
+  $cache_replacement_policy = 'lru'
+  $cache_dir_type = 'ufs'
+  $cache_dir_size = 1000
+  $maximum_object_size = '4096 MB'
+  $cache_swap_low = 90
+  $cache_swap_high = 95
+  $log_fqdn = off
+  $cachemgr_passwd = disable
+  $visible_hostname = undef
+  $snmp_port = undef
+  $adzapper = false
+
+  case $operatingsystem {
+    centos, redhat: {
+      $package = 'squid'
+      $service = 'squid'
+      $user = 'squid'
+      $group = 'squid'
+    }
+    ubuntu, debian: {
+      $package = 'squid3'
+      $service = 'squid3'
+      $user = 'proxy'
+      $group = 'proxy'
+    }
+  }
+
+  $cache_dir = "/var/spool/${package}"
+  $config_file = "/etc/${package}/squid.conf"
+
+}
+
+# vim: set ts=2 sw=2 sts=2 tw=0 et:
