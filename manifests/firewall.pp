d1040cbaa937645886b248b868cf2dadc574dad6
cleverlight <github.account@cleveraspaint.com>
2013-08-27 21:40:40 +0100

    changed out squid @firewall resource for @docommon::fireport

--- manifests/firewall.pp
+++ manifests/firewall.pp
@@ -4,11 +4,11 @@ class squid::firewall (
 
 ) {
 
-  @firewall { "0${http_port} Squid Caching Service TCP":
+  @docommon::fireport { "0${http_port} Squid Caching Service TCP":
     protocol => 'tcp',
     port     => $http_port,
   }
-  @firewall { "0${http_port} Squid Caching Service UDP":
+  @docommon::fireport { "0${http_port} Squid Caching Service UDP":
     protocol => 'udp',
     port     => $http_port,
   }
